9b9f52041cbf1aff98c42cf6d5c12af334beb6a9
Juan Dorado <fdorado985@gmail.com>
2020-09-21 16:15:52 -0500

    Decouple the responsibility on a single method to reuse

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -24,6 +24,32 @@ class ChecklistViewController: UITableViewController {
     // Do any additional setup after loading the view.
   }
 
+  func configureCheckmark(for cell: UITableViewCell, at indexPath: IndexPath) {
+    var isChecked = false
+    if indexPath.row == 0 {
+      row0checked = !row0checked
+      isChecked = row0checked
+    } else if indexPath.row == 1 {
+      row1checked = !row1checked
+      isChecked = row1checked
+    } else if indexPath.row == 2 {
+      row2checked = !row2checked
+      isChecked = row2checked
+    } else if indexPath.row == 3 {
+      row3checked = !row3checked
+      isChecked = row3checked
+    } else if indexPath.row == 4 {
+      row4checked = !row4checked
+      isChecked = row4checked
+    }
+
+    if isChecked {
+      cell.accessoryType = .checkmark
+    } else {
+      cell.accessoryType = .none
+    }
+  }
+
   // MARK: - Table View Data Source
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return 5
@@ -51,32 +77,8 @@ class ChecklistViewController: UITableViewController {
 
   // MARK: - Table View Delegate
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    if let cell = tableView.cellForRow(at: indexPath) {
-      var isChecked = false
-      if indexPath.row == 0 {
-        row0checked = !row0checked
-        isChecked = row0checked
-      } else if indexPath.row == 1 {
-        row1checked = !row1checked
-        isChecked = row1checked
-      } else if indexPath.row == 2 {
-        row2checked = !row2checked
-        isChecked = row2checked
-      } else if indexPath.row == 3 {
-        row3checked = !row3checked
-        isChecked = row3checked
-      } else if indexPath.row == 4 {
-        row4checked = !row4checked
-        isChecked = row4checked
-      }
-
-      if isChecked {
-        cell.accessoryType = .checkmark
-      } else {
-        cell.accessoryType = .none
-      }
-    }
-
+    let cell = tableView.cellForRow(at: indexPath) as! UITableViewCell
+    configureCheckmark(for: cell, at: indexPath)
     tableView.deselectRow(at: indexPath, animated: true)
   }
 }
