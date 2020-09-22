ce77e28148f4352fdb287964f99fb4da4652079e
Juan Dorado <fdorado985@gmail.com>
2020-09-22 11:21:52 -0500

    Connect the `UIBarButton`s with pop functionality on the `AddItemViewController`

---
+++ Checklists/AddItemViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by Juan Francisco Dorado Torres on 22/09/20.
+//
+
+import UIKit
+
+class AddItemViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    navigationItem.largeTitleDisplayMode = .never
+  }
+
+  // MARK: - Actions
+  @IBAction func cancel() {
+    navigationController?.popViewController(animated: true)
+  }
+
+  @IBAction func done() {
+    navigationController?.popViewController(animated: true)
+  }
+}
