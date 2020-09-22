7cf4c45dab62b58b1239f1212fe1092f1e38e38a
Juan Dorado <fdorado985@gmail.com>
2020-09-21 23:59:27 -0500

    Connect the `addItem` function to the button

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -50,6 +50,20 @@ class ChecklistViewController: UITableViewController {
     }
   }
 
+  // MARK: - Actions
+  @IBAction func addItem() {
+    let newRowIndex = items.count
+
+    let item = ChecklistItem()
+    item.text = "I am a new row"
+    item.checked = true
+    items.append(item)
+
+    let indexPath = IndexPath(row: newRowIndex, section: 0)
+    let indexPaths = [indexPath]
+    tableView.insertRows(at: indexPaths, with: .automatic)
+  }
+
   // MARK: - Table View Data Source
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return items.count
