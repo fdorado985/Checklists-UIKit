23b9e31a5a177dfaf765c93144dda10b72b0650d
Juan Dorado <fdorado985@gmail.com>
2020-10-01 10:52:00 -0500

    Trigger a local test notification

--- Checklists/AppDelegate.swift
+++ Checklists/AppDelegate.swift
@@ -6,11 +6,31 @@
 //
 
 import UIKit
+import UserNotifications
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
+    // Notification authorization
+    let center = UNUserNotificationCenter.current()
+    center.requestAuthorization(options: [.alert, .sound]) { granted, error in
+      if granted {
+        print("We have permission")
+      } else {
+        print("Permission denied")
+      }
+    }
+    center.delegate = self
+
+    let content = UNMutableNotificationContent()
+    content.title = "Hello!"
+    content.body = "I am a local notification"
+    content.sound = .default
+
+    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+    let request = UNNotificationRequest(identifier: "MyNotification", content: content, trigger: trigger)
+    center.add(request)
+
     return true
   }
 
@@ -26,5 +46,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // Called when the user discards a scene session.
     // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
     // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+  }
+
+  // MARK: - User Notification Delegates
+  func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+    print("Received local notification \(notification)")
   }
 }
